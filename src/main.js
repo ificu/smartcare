d3a0252313326e5382ca7e697c5b7b7acee26b0a
최범진 <bj.choi.ificu@gmail.com>
2019-12-01 16:02:01 +0900

    .

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import './registerServiceWorker'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
